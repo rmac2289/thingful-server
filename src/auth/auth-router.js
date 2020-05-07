cb76b0e87699bf73178ac5c067ff89824a30da20
Ross MacDonald <rmac2289@gmail.com>
2020-05-06 17:47:59 -0700

    added password authentication

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const AuthService = require('./auth-service')
 
-
 const authRouter = express.Router()
 const jsonBodyParser = express.json()
 
@@ -10,36 +9,37 @@ authRouter
     const { user_name, password } = req.body
     const loginUser = { user_name, password }
 
-    for (const [key,value] of Object.entries(loginUser))
-        if(value == null)
-            return res.status(400).json({
-                error: `Missing '${key}' in request body`
-            })
+    for (const [key, value] of Object.entries(loginUser))
+      if (value == null)
+        return res.status(400).json({
+          error: `Missing '${key}' in request body`
+        })
+
     AuthService.getUserWithUserName(
-        req.app.get('db'),
-        loginUser.user_name
-            )
-        .then(dbUser => {
-            if(!dbUser)
-                return res.status(400).json({
-                    error: `Incorrect user_name or password`,     
-                })
-                return AuthService.comparePasswords(loginUser.password, dbUser.password)
-        .then(compareMatch => {
-           if (!compareMatch)
-             return res.status(400).json({
-               error: 'Incorrect user_name or password',
-             })
+      req.app.get('db'),
+      loginUser.user_name
+    )
+      .then(dbUser => {
+        if (!dbUser)
+          return res.status(400).json({
+            error: 'Incorrect user_name or password',
+          })
+
+        return AuthService.comparePasswords(loginUser.password, dbUser.password)
+          .then(compareMatch => {
+            if (!compareMatch)
+              return res.status(400).json({
+                error: 'Incorrect user_name or password',
+              })
 
             const sub = dbUser.user_name
             const payload = { user_id: dbUser.id }
             res.send({
-                authToken: AuthService.createJwt(sub, payload),
+              authToken: AuthService.createJwt(sub, payload),
             })
-         })
-        })
-            .catch(next)
-            
+          })
+      })
+      .catch(next)
   })
 
 module.exports = authRouter
--- src/middleware/jwt-auth.js
+++ src/middleware/jwt-auth.js
@@ -1,14 +1,15 @@
 const AuthService = require('../auth/auth-service')
 
-   function requireAuth(req, res, next) {
+function requireAuth(req, res, next) {
   const authToken = req.get('Authorization') || ''
 
   let bearerToken
   if (!authToken.toLowerCase().startsWith('bearer ')) {
-       return res.status(401).json({ error: 'Missing bearer token' })
+    return res.status(401).json({ error: 'Missing bearer token' })
   } else {
     bearerToken = authToken.slice(7, authToken.length)
   }
+
   try {
     const payload = AuthService.verifyJwt(bearerToken)
 
@@ -19,8 +20,9 @@ const AuthService = require('../auth/auth-service')
       .then(user => {
         if (!user)
           return res.status(401).json({ error: 'Unauthorized request' })
-            req.user = user
-          next()
+
+        req.user = user
+        next()
       })
       .catch(err => {
         console.error(err)
@@ -29,7 +31,8 @@ const AuthService = require('../auth/auth-service')
   } catch(error) {
     res.status(401).json({ error: 'Unauthorized request' })
   }
-   }
+}
+
 module.exports = {
-    requireAuth
+  requireAuth,
 }
